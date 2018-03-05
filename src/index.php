f00ce285af1d516451a52db36f0cc93a08588123
thhuestc <thhuestc@163.com>
2018-03-05 10:48:03 +0000

    version2

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>CI/CD Demo: version1.0</title>
+        <title>CI/CD Demo: version2.0</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
         <style>body {margin-top: 40px; background-color: #333;}</style>
@@ -15,7 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>CI/CD Demo: version1.0!</h1>
+                <h1>CI/CD Demo: version2.0!</h1>
                 <h2>Congratulations</h2>
                 <p>Your ci/cd application is now running on a container in Amazon ECS.</p>
                 <?php
